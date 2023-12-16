6a01236589cdc84986706fdb3b36f1eab7a6d825
Queenie Wong <114438229+queeniewongpc@users.noreply.github.com>
2023-12-15 20:55:23 -0800

    enabled posting on homepage for logged-in users

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 import './Homepage.css';
 import Navbar from './Navbar';
 
 function Homepage() {
-  // show the post
   const [postListState, setPostListState] = useState([]);
+  const [newPostContent, setNewPostContent] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [userName, setUsername] = useState('');
+  const [joinTimestamp, setJoinTimestamp] = useState('');
+
+  async function getUsername() {
+    const response = await axios.get('/api/user/isLoggedIn');
+
+    if (response.data.username) {
+      setUsername(response.data.username);
+    }
+  }
 
   const getAllPost = async () => {
     const response = await axios.get('/api/blogpost/');
@@ -14,7 +26,9 @@ function Homepage() {
 
   useEffect(() => {
     getAllPost();
+    getUsername();
   }, []);
+
 
   const postComponent = [...postListState].reverse().map((post) => (
     <div key={post._id} className="postContainer">
@@ -26,10 +40,48 @@ function Homepage() {
     </div>
   ));
 
+  function updatePostContent(event) {
+    setNewPostContent(event.target.value);
+  }
+
+  async function makeNewPost() {
+    const newPost = {
+      owner: userName,
+      text: newPostContent,
+    };
+
+    await axios.post('/api/blogpost', newPost);
+
+    await getAllPost();
+
+    setNewPostContent('');
+  }
+
+  let usernameMessage = <div>Loading...</div>;
+  if (userName) {
+    usernameMessage = <div>Logged in as {userName}</div>;
+  }
+
   return (
     <div>
       <div className="home-container">
-        <Navbar />
+        {userName ? <Navbar isLoggedIn={isLoggedIn} /> : <Navbar />}
+  
+        {userName && (
+          <div className="container">
+            <h3>Make a new post</h3>
+            <div>Content: </div>
+            <input
+              className="inputField"
+              onInput={updatePostContent}
+              value={newPostContent}
+            />
+            <button className="submitButton" onClick={makeNewPost}>
+              Submit
+            </button>
+          </div>
+        )}
+
         <div className="blog-posts">{postComponent}</div>
       </div>
     </div>
